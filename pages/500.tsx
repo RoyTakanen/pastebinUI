74d52cd3b9b13f814323889a1917fdbb17b943bf
Midka <me@midka.dev>
2022-10-01 17:26:38 +0300

    refactor: change 404.js and 500.js -> .tsx

--- pages/500.tsx
+++ pages/500.tsx
@@ -1,5 +1,5 @@
-import { HeaderMenu } from '../components/Header/HeaderMenu';
 import { Error } from '../components/Error/Error';
+import { HeaderMenu } from '../components/Header/HeaderMenu';
 
 export default function Custom500() {
   const links = [
@@ -13,9 +13,9 @@ export default function Custom500() {
       {/* @ts-ignore */}
       <HeaderMenu links={links} />
       <Error
-        errorCode={404}
-        errorTitle='Sivua ei ole olemassa'
-        errorText='Sivu on joko poistettu tai se ei ole koskaan ollut olemassa. Tarkasta osoite uudestaan tai siirry etusivulle.'
+        errorCode={500}
+        errorTitle="Palvelinvirhe"
+        errorText="Palvelimelta tuli virheellinen vastaus"
       />
     </>
   );
