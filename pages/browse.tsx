efb792e99326b5f525376198ebba5aa4ab9c8d84
Midka <me@midka.dev>
2022-10-01 23:32:56 +0300

    remove: search from browse temporarily

--- pages/browse.tsx
+++ pages/browse.tsx
@@ -1,126 +1,30 @@
-import { Checkbox, Container, Group, Input, Select, Stack, Title } from '@mantine/core';
-import { showNotification } from '@mantine/notifications';
-import { IconArrowsSort, IconSearch, IconX } from '@tabler/icons';
-import { useEffect, useState } from 'react';
-
+import { Container, Stack, Text, Title } from '@mantine/core';
+import { Error } from '../components/Error/Error';
 import { PasteCardVertical } from '../components/PasteCard/PasteCardVertical';
+import { useGetLatestPastes } from '../data/paste.api';
 
 export default function Paste() {
-  const latestDefault = [
-    {
-      author: 'Tuntematon Sotilas',
-      date: '2022-08-04T12:57:22.810Z',
-      title: 'Python-esimerkki',
-      language: 'python',
-      id: 'XXXXXX',
-      meta: {
-        size: 1,
-        views: 1,
-      },
-    },
-  ];
+  const { data: latest, error } = useGetLatestPastes();
 
-  const [latest, setLatest] = useState(latestDefault);
-  const [loadLatest, setLoadLatest] = useState(false);
-
-  const fetchPastes = (sorting: string, inverted: boolean, searchTerm: string = '') => {
-    fetch(
-      `${process.env.NEXT_PUBLIC_API_ENDPOINT}/pastes?sorting=${
-        inverted ? '-' : ''
-      }${sorting}&title=${searchTerm}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.error) {
-          showNotification({
-            color: 'red',
-            message: data.error,
-            disallowClose: true,
-            icon: <IconX size={20} />,
-          });
-        } else {
-          let newLatest = structuredClone(latest);
-          newLatest = data;
-          setLatest(newLatest);
-          setLoadLatest(true);
-        }
-      });
-  };
-
-  if (!loadLatest) {
-    fetchPastes('meta.views', true);
+  if (error) {
+    return <Error errorCode={error.status} errorTitle={error.title} errorText={error.message} />;
   }
 
-  const author = {
-    name: 'tuntematon',
-    avatar:
-      'https://images.unsplash.com/photo-1534294668821-28a3054f4256?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80',
-  };
-
-  const [sorting, setSorting] = useState<string | null>(null);
-  const [inverted, setInverted] = useState(false);
-  const [searchTerm, setSearchTerm] = useState('');
-
-  useEffect(() => {
-    fetchPastes(sorting != null ? sorting : '', inverted, searchTerm);
-  }, [sorting, inverted, searchTerm]);
+  if (!latest) return <Text color="dimmed">Ei tietoa</Text>;
 
   return (
     <Container px={0}>
-      <Title>Selaa liitteitä</Title>
-      {/* This is rendered again when input changes (pls fix) */}
-      <Input
-        mt={30}
-        icon={<IconSearch />}
-        placeholder="Hae liitteitä"
-        size="md"
-        key="searchTerm"
-        autoFocus
-        value={searchTerm}
-        onChange={(event: any) => setSearchTerm(event.currentTarget.value)}
-      />
-      <Group mb="xs">
-        <Container size="sm" sx={{ flex: 1 }}>
-          <Select
-            mt={20}
-            transition="pop-top-left"
-            transitionDuration={80}
-            transitionTimingFunction="ease"
-            value={sorting}
-            onChange={setSorting}
-            label="Lajitteluperuste"
-            placeholder="Valitse yksi"
-            icon={<IconArrowsSort size={14} />}
-            defaultValue="meta.views"
-            data={[
-              { value: 'meta.views', label: 'Katselukerrat' },
-              { value: 'meta.size', label: 'Koko' },
-              { value: 'date', label: 'Päivämäärä' },
-            ]}
-          />
-        </Container>
-        <Container size="sm" pr={90} mt={40}>
-          <Checkbox
-            label="Käänteinen"
-            checked={inverted}
-            onChange={(event) => setInverted(event.currentTarget.checked)}
-          />
-        </Container>
-      </Group>
+      <Title>Hae pasteja</Title>
       <Stack mt={40}>
         {latest.map((latestPaste) => (
           <PasteCardVertical
-            language={
-              /* @ts-ignore */
-              latestPaste.programmingLanguage ? latestPaste.programmingLanguage : 'teksti'
-            }
+            programmingLanguage={latestPaste.programmingLanguage}
             title={latestPaste.title ? latestPaste.title : 'Nimetön...'}
             date={new Date(latestPaste.date).toLocaleDateString('fi-FI')}
-            author={author}
             id={latestPaste.id}
-            size={latestPaste.meta.size}
-            views={latestPaste.meta.views}
             key={latestPaste.id}
+            meta={latestPaste.meta}
+            author={latestPaste.author}
           />
         ))}
       </Stack>
